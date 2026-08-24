74ee333914bd21cb043b7c7df827774bfc3f56ed
Armando17124 <armando17124@outlook.com>
2026-08-23 21:15:30 -0600

    animacion

--- escenas/main.gd
+++ escenas/main.gd
@@ -1,14 +1,49 @@
 extends Node3D
 
-# Cargar las escenas correspondientes (Verifica que las rutas coincidan exactamente)
 var hud_escena = preload("res://hud.tscn")
 var pausa_escena = preload("res://pause_menu.tscn")
 
-func _ready():
-	# Instanciar e insertar el HUD
+@onready var animador: AnimationPlayer = $recorridocamara
+@onready var camara_cinematica: Camera3D = $Path3D/PathFollow3D/Camera3D
+@onready var jugador: CharacterBody3D = $Jugador
+
+# Esta variable estática no se borra al morir y reiniciar el nivel
+static var ya_vio_cinematica: bool = false
+
+func _ready() -> void:
 	var hud_instancia = hud_escena.instantiate()
 	add_child(hud_instancia)
 	
-	# Instanciar e insertar el Menú de Pausa
 	var pausa_instancia = pausa_escena.instantiate()
 	add_child(pausa_instancia)
+	
+	if not ya_vio_cinematica:
+		# --- PRIMERA VEZ: REPRODUCE LA CINEMÁTICA ---
+		hud_instancia.visible = false
+		
+		if animador and camara_cinematica:
+			camara_cinematica.current = true
+			
+			if jugador:
+				jugador.set_physics_process(false)
+				jugador.set_process_unhandled_input(false)
+			
+			animador.play("recorridocamara")
+			await animador.animation_finished
+			
+			if jugador and jugador.has_node("Camera3D"):
+				jugador.get_node("Camera3D").current = true
+			
+			if jugador:
+				jugador.set_physics_process(true)
+				jugador.set_process_unhandled_input(true)
+				
+			hud_instancia.visible = true
+			
+			# Marcamos que ya se vio la cinemática
+			ya_vio_cinematica = true
+	else:
+		# --- SI MURIÓ Y REINICIÓ: SALTA DIRECTO AL JUEGO ---
+		hud_instancia.visible = true
+		if jugador and jugador.has_node("Camera3D"):
+			jugador.get_node("Camera3D").current = true
---
+++ estado_juego.gd
@@ -0,0 +1,4 @@
+extends Node
+
+# Variable global para recordar si ya vimos la cinemática
+var cinematica_vista: bool = false
